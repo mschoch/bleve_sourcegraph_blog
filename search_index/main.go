b3a55ee0f43b286f0796e72658a5cf1cfd49d9a8
Marty Schoch <marty.schoch@gmail.com>
2015-03-04 10:09:16 -0500

    add search index

---
+++ search_index/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/blevesearch/bleve"
+)
+
+type Person struct {
+	Name string
+}
+
+func main() {
+	index, err := bleve.Open("people.bleve")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	query := bleve.NewTermQuery("marty")
+	request := bleve.NewSearchRequest(query)
+	result, err := index.Search(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result)
+}
